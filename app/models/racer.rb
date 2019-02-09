23da48d50adf7b960dc228bde92abc7d3b10a045
hamza0867 <hamza0867@gmail.com>
2019-02-09 15:58:00 +0100

    Pass CRUD Model 4

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,5 +1,17 @@
 # Class representing a racer
 class Racer
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+  def initialize(params = {})
+    @id = params[:_id].nil? ? params[:id] : params[:_id].to_s
+    @number = params[:number].to_i
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @gender = params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+  end
+
   # convenience method for access to client in console
   def self.mongo_client
     Mongoid::Clients.default
@@ -12,5 +24,11 @@ class Racer
 
   def self.all(prototype = {}, sort = { number: 1 }, skip = 0, limit = nil)
     collection.find(prototype).sort(sort).skip(skip).limit(limit || 0)
+  end
+
+  def self.find(id)
+    val = id.is_a?(BSON::ObjectId) ? id : BSON::ObjectId(id)
+    result = collection.find(_id: val).first
+    result.nil? ? nil : Racer.new(result)
   end
 end
