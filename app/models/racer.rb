a404fbc318101a2f367987dbdd9916481c693bac
hamza0867 <hamza0867@gmail.com>
2019-02-09 11:10:58 +0100

    Pass CRUD Model Methods 1

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,3 +1,4 @@
+# Class representing a racer
 class Racer
   # convenience method for access to client in console
   def self.mongo_client
@@ -7,5 +8,9 @@ class Racer
   # convenience method for access to zips collection
   def self.collection
     mongo_client['racers']
+  end
+
+  def self.all(prototype = {}, sort = { number: 1 }, skip = 0, limit = nil)
+    collection.find(prototype).sort(sort).skip(skip).limit(limit || 0)
   end
 end
